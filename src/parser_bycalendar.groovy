a73b5f4de76fcef63bd87d89fc0b3ab155515b31
Sirish <sirishk99@gmail.com>
2021-05-29 20:49:25 +0530

    slack webhook pushed

--- src/parser_bycalendar.groovy
+++ src/parser_bycalendar.groovy
@@ -17,7 +17,7 @@ String date, time
 String msg = ""
 def districtMap = ["294": "BBMP", "265": "Bangalore Urban", "276": "Bangalore Rural"]
 //def districtMap = ["294": "BBMP", "265": "Bangalore Urban", "276": "Bangalore Rural", "145" : "East Delhi"]
-String slackWebhook="https://hooks.slack.com/services/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"
+String slackWebhook="https://hooks.slack.com/services/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"
 def min_age = 45
 def dose = 2
 int sleepSeconds = 180
